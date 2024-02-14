098cfcdda36046a012b6c03d2787b66cb8caa55b
hai hoang <65638605+haih19@users.noreply.github.com>
2024-02-15 05:14:03 +0700

    feature: sidebar

---
+++ app/(dashboard)/_components/sidebar/list.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useOrganizationList } from "@clerk/nextjs";
+import { Item } from "./item";
+
+export const List = () => {
+  const { userMemberships } = useOrganizationList({
+    userMemberships: {
+      infinite: true
+    }
+  });
+  return (
+    <ul className="space-y-4">
+      {userMemberships.data?.map((member) => (
+        <Item
+          key={member.organization.id}
+          id={member.organization.id}
+          name={member.organization.name}
+          imageUrl={member.organization?.imageUrl}
+        />
+      ))}
+    </ul>
+  );
+};
